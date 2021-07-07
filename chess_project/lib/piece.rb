4ddd1e42b696262915344a31f9b80593e8ab4545
bisdeep <[deepbiswasrepo@gmail.com]>
2021-07-07 10:41:19 -0400

    Chess

--- chess_project/lib/piece.rb
+++ chess_project/lib/piece.rb
@@ -1,3 +1,45 @@
-class Piece
+module Slideable
+
+end
+
+module Steppable
     
+end
+
+class Piece
+    def initialize(color, board, pos)
+        @color = color #should be a symbol
+        @board = board #should be a board instance
+        @pos = pos #should be an array
+    end
+
+    def moves
+        # places = []
+
+        # return places
+    end
+
+    def to_s
+
+    end
+
+    def empty?
+
+    end
+
+    def pos=(val)
+
+    end
+
+    def Symbol
+
+    end
+
+    private
+
+    def move_into_check?(end_pos)
+
+    end
+
+
 end
